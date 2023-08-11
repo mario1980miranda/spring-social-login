0d82d74083d4027ee97f675a4cc66300a8e6e7d0
Mario Miranda <mario_chan15@hotmail.com>
2023-08-11 08:14:10 -0400

    impl new @ModelAttribute page injection

--- src/main/java/com/code/truck/oauth/springsociallogin/controllers/AppUserController.java
+++ src/main/java/com/code/truck/oauth/springsociallogin/controllers/AppUserController.java
@@ -2,8 +2,8 @@ package com.code.truck.oauth.springsociallogin.controllers;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.code.truck.oauth.springsociallogin.entities.AppUser;
@@ -12,9 +12,13 @@ import com.code.truck.oauth.springsociallogin.entities.AppUser;
 @RequestMapping("/user")
 public class AppUserController {
 
+    @ModelAttribute
+    public AppUser appUser(@AuthenticationPrincipal AppUser appUser) {
+        return appUser;
+    }
+
     @GetMapping
-    public String showUserInfo(@AuthenticationPrincipal AppUser appUser, Model model) {
-        model.addAttribute("appUser", appUser);
+    public String showUserInfo() {
         return "app-user/user";
     }
 }
